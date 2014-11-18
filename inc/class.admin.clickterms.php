7c7ae2f5da8ac782f43357b4d4670491943f593f
Chaika <chaika.design@gmail.com>
2014-11-19 05:51:40 +0900

    trim

--- inc/class.admin.clickterms.php
+++ inc/class.admin.clickterms.php
@@ -9,7 +9,7 @@ class SimpleTags_Admin_ClickTags {
 	public function __construct() {
 		// Ajax action, JS Helper and admin action
 		add_action('wp_ajax_'.'simpletags', array(__CLASS__, 'ajax_check'));
-		
+
 		// Box for post/page
 		add_action('admin_menu', array(__CLASS__, 'admin_menu'), 1);
 
@@ -25,20 +25,20 @@ class SimpleTags_Admin_ClickTags {
 	 */
 	public static function admin_enqueue_scripts() {
 		global $pagenow;
-		
+
 		wp_register_script('st-helper-click-tags', STAGS_URL.'/assets/js/helper-click-tags.js', array('jquery', 'st-helper-add-tags'), STAGS_VERSION);
 		wp_localize_script('st-helper-click-tags', 'stHelperClickTagsL10n', array( 'show_txt' => __('Display click tags', 'simpletags'), 'hide_txt' => __('Hide click tags', 'simpletags') ) );
-		
+
 		// Register location
 		$wp_post_pages = array('post.php', 'post-new.php');
 		$wp_page_pages = array('page.php', 'page-new.php');
-		
+
 		// Helper for posts/pages
 		if ( in_array($pagenow, $wp_post_pages) || ( in_array($pagenow, $wp_page_pages) && is_page_have_tags() ) ) {
 			wp_enqueue_script('st-helper-click-tags');
 		}
 	}
-	
+
 	/**
 	 * Register metabox
 	 *
@@ -50,7 +50,7 @@ class SimpleTags_Admin_ClickTags {
 		if ( is_page_have_tags() )
 			add_meta_box('st-clicks-tags', __('Click tags', 'simpletags'), array(__CLASS__, 'metabox'), 'page', 'advanced', 'core');
 	}
-	
+
 	/**
 	 * Put default HTML for people without JS
 	 *
@@ -60,7 +60,7 @@ class SimpleTags_Admin_ClickTags {
 	public static function metabox() {
 		echo SimpleTags_Admin::getDefaultContentBox();
 	}
-	
+
 	/**
 	 * Ajax Dispatcher
 	 *
@@ -72,7 +72,7 @@ class SimpleTags_Admin_ClickTags {
 			self::ajax_click_tags();
 		}
 	}
-	
+
 	/**
 	 * Display a span list for click tags
 	 *
@@ -82,16 +82,16 @@ class SimpleTags_Admin_ClickTags {
 	public static function ajax_click_tags() {
 		status_header( 200 ); // Send good header HTTP
 		header("Content-Type: text/html; charset=" . get_bloginfo('charset'));
-		
+
 		if ((int) wp_count_terms('post_tag', 'ignore_empty=false') == 0 ) { // No tags to suggest
 			echo '<p>'.__('No terms in your WordPress database.', 'simpletags').'</p>';
 			exit();
 		}
-		
+
 		// Prepare search
 		$search = ( isset($_GET['q']) ) ? trim(stripslashes($_GET['q'])) : '';
         $post_id = ( isset($_GET['post_id']) ) ? intval($_GET['post_id']) : 0;
-		
+
 		// Order tags before selection (count-asc/count-desc/name-asc/name-desc/random)
 		$order_click_tags = strtolower(SimpleTags_Plugin::get_option_value('order_click_tags'));
 		$order_by = $order = '';
@@ -117,7 +117,7 @@ class SimpleTags_Admin_ClickTags {
 				$order = 'ASC';
 			break;
 		}
-		
+
 		// Get all terms, or filter with search
 		$terms = SimpleTags_Admin::getTermsForAjax( 'post_tag', $search, $order_by, $order );
 		if ( empty($terms) || $terms == false ) {
